90d82ae585b53c41e615ba0627594c17c7295330
veraefita <veraefita05@gmail.com>
2024-09-25 16:37:55 +0700

    jobsheet 5 prak 10

---
+++ jobsheet5_php2/time.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+    </head>
+    <body>
+        <h3> Time </h3>
+    <?php
+        date_default_timezone_set ("asia/jakarta");
+        echo date ("h:i:sa");
+
+    ?>
+</body>
+</html>
