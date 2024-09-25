9fccac6e3dc1e331935470ec825e2a6b72cfd59c
veraefita <veraefita05@gmail.com>
2024-09-25 16:29:23 +0700

    jobsheet 5 praktikum 1

---
+++ jobsheet5_php2/array_1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<h2>Array Terindeks</h2>
+<?php
+    $Listdosen = ["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
+
+    // Menggunakan loop foreach untuk menampilkan elemen array
+    foreach ($Listdosen as $dosen) {
+        echo $dosen . "<br>";
+    }
+?>
+    
+</body>
+</html>
+
+
